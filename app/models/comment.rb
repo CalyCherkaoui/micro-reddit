10e22581a48e40defe8e21e9f633ba614dcacca0
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-08-11 01:27:26 +0300

    Fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
-    validates :body, presence:true, length: {minimun: 1}, too_short: 'minimun lenght for a comment is one character'
+    validates :body, presence:true, length: { minimun: 1 }, too_short: 'minimun lenght for a comment is one character'
 end
