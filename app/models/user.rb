80290d22e47be699c140b08ba694e9ebf434712f
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-08-11 01:29:47 +0300

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates_length_of :name, within: 6..20, too_long: 'pick a shorter name', too_short: 'pick a longer name'
-
   validates_uniqueness_of :email
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-  
   validates :password, presence: true, length: { minimum: 6 }
 end
