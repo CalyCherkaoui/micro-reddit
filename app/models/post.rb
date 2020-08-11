d52e633d769f04c120336d91bca989fb16c23335
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-08-11 10:00:51 -0500

    Fixing Code review errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :title, presence: true, length: { minimun: 5 }, too_short: 'Get serious!!!'
-  validates :body, presence: true, length: { minimun: 5 }, too_short: 'Come on!!! This is toooooo short'
+  validates :title, presence: true, length: { minimum: 5 }, too_short: 'Get serious!!!'
+  validates :body, presence: true, length: { minimum: 5 }, too_short: 'Come on!!! This is toooooo short'
 end
